8e36fe76f6d26fff1305d14a9e374ee072335244
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-24 11:19:43 +0400

    Mistake in comments

--- loop.py
+++ loop.py
@@ -3,7 +3,7 @@ import vk_api
 from get_vk_messages import get-message
 from parse_messages.py import parse
 #vk = VKontakte object
-#last-message-id = Last message ID, what taken from VK object
+#last-message-id = Last message ID, which taken from VK object
 
 def start-loop():
     while True: #Infinity loop
