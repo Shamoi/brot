f3e2f925a17986a42fe27684322f074e937c22ce
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-24 20:16:45 +0400

    Fix captcha error

--- out_vk_messages.py
+++ out_vk_messages.py
@@ -2,6 +2,7 @@ def send_messages(messages, vk, chat_id):
     for message in messages["text"]:
         response = vk.method('messages.send',
                               {"chat_id" : chat_id, "message" : message})
+        time.sleep(0.5)
     try:
         for pic in messages["photo"]:
             response = vk.method('messages.send',
