a12117dc794e47a56c5b60422a10cea0d8e67601
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-24 16:56:06 +0400

    Add message.lower to check function

--- modules/time/check.py
+++ modules/time/check.py
@@ -3,7 +3,7 @@ import re
 time_vtk = re.compile('время|time')
 
 def check(message):
-    if time_vtk.match(message):
+    if time_vtk.match(message.lower()):
         return True
     else:
         return False
