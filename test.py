7c062b509988e62756d99bd537745a689c7f6c81
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-24 09:30:58 +0400

    Small changes in tests

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from modules.time import tests #from each module taking tests
 
-def doTests():
-    results = ["Тестирование"]
+def doCommandsTests():
+    results = ["Тестирование команд:"]
     results.append(tests.tests())
     return results
 
 if __name__ == '__main__':
-    results = doTests()
+    results = doCommandsTests()
     for result in results:
         print(result)
