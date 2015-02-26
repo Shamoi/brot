dc682cda139969ee9fedc180690ebd2b75c16404
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-26 10:40:59 +0400

    Fix wiki module

--- modules/wiki/get.py
+++ modules/wiki/get.py
@@ -4,7 +4,7 @@ wikipedia.set_lang("ru")
 
 def get(message):
     try:
-        wikipedia_is_in = wikipedia.summary(message["text"].replace("?", "").split(" ")[10:], sentences = 1)
+        wikipedia_is_in = wikipedia.summary(message["text"][10:], sentences = 1)
     except wikipedia.exceptions.DisambiguationError as e:
         message["text"] = "что такое " +  e.options[0]
     except wikipedia.exceptions.WikipediaException:
