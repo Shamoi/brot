26027cd49bb8b731df8c03dac9a4fba09784dfb5
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-26 11:10:32 +0400

    Fix sending empty messages

--- out_vk_messages.py
+++ out_vk_messages.py
@@ -2,9 +2,9 @@ import time
 
 def send_messages(messages, vk, chat_id):
     for message in messages["text"]:
-        response = vk.method('messages.send',
+        if message:
+            response = vk.method('messages.send',
                               {"chat_id" : chat_id, "message" : message})
-        time.sleep(0.5)
     try:
         for pic in messages["photos"]:
             response = vk.method('messages.send',
