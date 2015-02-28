30aa15d1524d04a805d3f75d538486f6c4b04270
Seva Zhidkov <zhidkovseva@gmail.com>
2015-02-28 13:31:10 +0400

    Добавлена обработка вложений к сообщению

--- get_vk_messages.py
+++ get_vk_messages.py
@@ -14,7 +14,18 @@ def get_message(vk, chat_id, last_message_id, ignored_users):
             message = {"time" : response["items"][0]["date"], "sender" : response["items"][0]["user_id"],
                        "chat_id" : "", "id" : response["items"][0]["id"], "text" : response["items"][0]["body"]}
 
-        
+
+        if "attachments" in response["items"][0]:
+            if "photo" in response["items"][0]["attachments"][0]:
+                message.update({"attachment" : "photo" + str(response["items"][0]["attachments"][0]["photo"]["owner_id"]) + "_"
+                                + str(response["items"][0]["attachments"][0]["photo"]["id"])})
+            elif "audio" in response["items"][0]["attachments"][0]:
+                message.update({"attachment" : "audio" + str(response["items"][0]["attachments"][0]["audio"]["owner_id"]) + "_"
+                                + str(response["items"][0]["attachments"][0]["audio"]["id"])})
+            elif "video" in response["items"][0]["attachments"][0]:
+                message.update({"attachment" : "audio" + str(response["items"][0]["attachments"][0]["video"]["owner_id"]) + "_"
+                                + str(response["items"][0]["attachments"][0]["video"]["id"])})
+
         if response["items"][0]["user_id"] in ignored_users: #Check, if sender are ignored - ignore message
             return None
 
