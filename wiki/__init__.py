1b49920c542a7c990d68ce0d6e1d772d54251a90
Seva Zhidkov <zhidkovseva@gmail.com>
2015-03-23 18:33:10 +0400

    Added wiki module

---
+++ wiki/__init__.py
@@ -0,0 +1,17 @@
+import wikipedia
+import re
+
+wikipedia.set_lang('ru')
+expression = re.compile('(ч|к)то так(ой|ая|ое|ие) ([^?]+)\??', re.IGNORECASE)
+
+
+def get(message):
+    query = expression.search(message['text']).group(3)
+    try:
+        wiki_answer = wikipedia.summary(query, 5)
+    except wikipedia.exceptions.DisambiguationError as error:
+        wiki_answer = wikipedia.summary(error.options[0], 5)
+    except Exception as error:
+        print(error)
+        return {'text': "Произошла ошибка. Сообщите Севе.", 'attachments': []}
+    return {'text': wiki_answer, 'attachments': []}
