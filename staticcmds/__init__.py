58a9f858c090cf4e68ff5d123e7df0efad8b2a3c
Seva Zhidkov <zhidkovseva@gmail.com>
2015-03-24 09:10:33 +0400

    Speed optimization in staticcmds module

--- staticcmds/__init__.py
+++ staticcmds/__init__.py
@@ -1,6 +1,5 @@
 import re
 import redis
-import json
 
 static_cmds = redis.StrictRedis(host='localhost', port=6379, db=0)
 expression = re.compile('"(.+)" ?- ?"(.+)"', re.IGNORECASE)
@@ -10,15 +9,15 @@ def get(message):
     if read_command:
         read_command = read_command.decode('utf-8')
         return {
-            'text': json.loads(read_command)['text'],
-            'attachments': json.loads(read_command)['attachments']
+            'text': read_command['text'],
+            'attachments': read_command['attachments']
         }
     else:
         adding_command = expression.search(message['text'])
         if adding_command:
             static_cmds.set(
                 adding_command.group(1).lower(),
-                json.dumps({
+                {
                     'text': adding_command.group(2),
                     'attachments': message['attachments']
                 })
